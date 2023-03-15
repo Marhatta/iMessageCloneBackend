d46784211e5ca25396cbb7bd6248736375575b60
Marhatta <vishal.marhatta@gmail.com>
2023-03-16 02:36:46 +0530

    created mutation for createUsername

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,10 +1,48 @@
+import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
+
 const resolvers = {
-    Query:{
-        searchUsers:()=>{}
-    },
-    Mutation:{
-        createUsername:()=>{}
+  Query: {
+    searchUsers: () => {}
+  },
+  Mutation: {
+    createUsername: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ): Promise<CreateUsernameResponse> => {
+      const { username } = args;
+      const { prisma, session } = context;
+
+      const { id: userId } = session?.user;
+
+      try {
+        if (!session?.user) {
+          return {
+            error: "Not authorized"
+          };
+        }
+
+        // check that username is not taken
+        const existingUser = await prisma.user.findUnique({
+          where: { username }
+        });
+
+        if (existingUser) {
+          return {
+            error: " Username already taken. Try another"
+          };
+        }
+
+        // update user
+        await prisma.user.update({ where: { id: userId }, data: { username } });
+
+        return { success: true };
+      } catch (error: any) {
+        console.log("create username error", error);
+        return { error: error?.message };
+      }
     }
-}
+  }
+};
 
 export default resolvers;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
-import http from 'http';
-import resolvers from './graphql/resolvers';
-import typeDefs from './graphql/typeDefs';
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { PrismaClient } from "@prisma/client";
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageLocalDefault
+} from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import * as dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import { getSession } from "next-auth/react";
+import resolvers from "./graphql/resolvers";
+import typeDefs from "./graphql/typeDefs";
+import { GraphQLContext, Session } from "./util/types";
 
 async function main() {
+  dotenv.config();
   // Required logic for integrating with Express
   const app = express();
   // Our httpServer handles incoming requests to our Express app.
@@ -17,30 +26,47 @@ async function main() {
   const schema = makeExecutableSchema({
     typeDefs,
     resolvers
-  })
+  });
 
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true
+  };
+
+  // Context parameters
+  const prisma = new PrismaClient();
+  // const pubsub
   // Same ApolloServer initialization as before, plus the drain plugin
   // for our httpServer.
   const server = new ApolloServer({
     schema,
     csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    cache: "bounded",
+    context: async ({ req, res }): Promise<GraphQLContext> => {
+      const session = (await getSession({ req })) as Session;
+      return { session, prisma };
+    },
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true })
+    ]
   });
 
   // More required logic for integrating with Express
   await server.start();
   server.applyMiddleware({
     app,
-
+    cors: corsOptions,
     // By default, apollo-server hosts its GraphQL endpoint at the
     // server root. However, *other* Apollo Server packages host it at
     // /graphql. Optionally provide this to match apollo-server.
-    path: '/graphql',
+    path: "/graphql"
   });
 
   // Modified server startup
-  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise<void>(resolve =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
